217ec9bc422cbaa2023ca29d9e1f75ec83686e3d
MD Ratul <tahid7266@gmail.com>
2026-03-31 01:59:04 +0600

    setup project environment and configuration, starting development

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Products from './data/products.json';
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-center text-4xl font-bold'>hello bangladesh!</h1>
+    </>
+  )
+}
+
+export default App
